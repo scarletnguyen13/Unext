d20610fa92c6fc6094e46ab88c8e04c1149b273d
Scarlet Nguyen <scarletnguyen@Scarlets-MacBook-Pro.local>
2019-02-03 15:32:48 -0800

    Fix eslint errors

---
+++ src/assets/mouth/index.js
@@ -0,0 +1,27 @@
+import mouth1 from './mouth-1.png';
+import mouth2 from './mouth-2.png';
+import mouth3 from './mouth-3.png';
+import mouth4 from './mouth-4.png';
+import mouth5 from './mouth-5.png';
+import mouth6 from './mouth-6.png';
+import mouth7 from './mouth-7.png';
+import mouth8 from './mouth-8.png';
+import mouth9 from './mouth-9.png';
+import mouth10 from './mouth-10.png';
+import mouth11 from './mouth-11.png';
+import mouth12 from './mouth-12.png';
+
+export default [
+  mouth1,
+  mouth2,
+  mouth3,
+  mouth4,
+  mouth5,
+  mouth6,
+  mouth7,
+  mouth8,
+  mouth9,
+  mouth10,
+  mouth11,
+  mouth12,
+];
--- src/components/ItemsContainer/ItemsContainer.js
+++ src/components/ItemsContainer/ItemsContainer.js
@@ -2,59 +2,61 @@ import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Item from '../Item/Item';
 
+import mouths from '../../assets/mouth/index';
+
 import './ItemsContainer.css';
 
-const array = [1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12];
-
-var ID = function () {
+function ID() {
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
   // after the decimal.
-  return '_' + Math.random().toString(36).substr(2, 9);
-};
-
+  return `_${Math.random().toString(36).substr(2, 9)}`;
+}
 
 class ItemsContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      items: [1, 2, 3, 4, 5],
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    alert('Click!!!!');
+    alert(this.state.items); // eslint-disable-line no-alert
   }
 
   render() {
     let cols = [];
-    let rows = array.map(i => {
+    const rows = mouths.map((src) => {
       cols.push(
         <Col xs={7} sm={4} key={ID()}>
-          <Item 
-            src={require(`../../assets/mouth/mouth-${i}.png`)}
+          <Item
+            src={src}
             onClick={this.handleClick}
             key={ID()}
           />
-        </Col>);
+        </Col>,
+      );
       if (cols.length >= 3) {
-        let temp = cols.slice();
-        cols = [];  
-        return <Row key={ID()}>{temp}</Row>
-      } else {
-        return (null);
+        const temp = cols.slice();
+        cols = [];
+        return <Row key={ID()}>{temp}</Row>;
       }
+      return (null);
     });
-    if(cols.length > 0) rows.push(<Row key={ID()}>{cols}</Row>);
-    
-    return(
-      <div className='box-field'>
-        <div id='items-container'>
+    if (cols.length > 0) rows.push(<Row key={ID()}>{cols}</Row>);
+
+    return (
+      <div className="box-field">
+        <div id="items-container">
           <Grid fluid>
-           {rows}
+            {rows}
           </Grid>
         </div>
       </div>
     );
   }
-} 
+}
 
 export default ItemsContainer;
